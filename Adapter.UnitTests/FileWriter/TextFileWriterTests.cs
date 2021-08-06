45daed084b5d9d668898b14f896c027d62caa89b
Andriy Shyrokoryadov <andrey.sh@wp.pl>
2021-08-06 19:34:54 +0200

    Added Adapter design pattern example

---
+++ Adapter.UnitTests/FileWriter/TextFileWriterTests.cs
@@ -0,0 +1,34 @@
+﻿using Adapter.FileWriter;
+using Adapter.UnitTests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Adapter.UnitTests.FileWriter
+{
+    [TestClass]
+    public class TextFileWriterTests
+    {
+        [TestMethod]
+        public void GIVEN_not_empty_content_WHEN_write_is_invoked_THEN_no_exception_is_thrown()
+        {
+            var systemUnderTests = new TextFileWriter("test.txt");
+            var actual = systemUnderTests.Write(ContentHelper.GetFilled());
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void GIVEN_empty_content_WHEN_write_is_invoked_THEN_no_exception_is_thrown()
+        {
+            var systemUnderTests = new TextFileWriter("test.txt");
+            var actual = systemUnderTests.Write(ContentHelper.GetEmpty());
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void GIVEN_null_content_WHEN_publish_is_invoked_THEN_exception_is_thrown()
+        {
+            var systemUnderTests = new TextFileWriter("test.txt");
+            var actual = systemUnderTests.Write(ContentHelper.GetNull());
+            Assert.IsFalse(actual);
+        }
+    }
+}
---
+++ Adapter/Adapters/PublisherToWriterAdapter.cs
@@ -0,0 +1,33 @@
+﻿using Adapter.FilePublisher;
+using Adapter.FileWriter;
+using System;
+using System.Collections.Generic;
+
+namespace Adapter.Adapters
+{
+    public class PublisherToWriterAdapter : IFileWriter
+    {
+        private IFilePublisher _filePublisher;
+
+        public PublisherToWriterAdapter(IFilePublisher filePublisher)
+        {
+            _filePublisher = filePublisher;
+        }
+
+        public bool Write(IEnumerable<string> contents)
+        {
+            try
+            {
+                Console.WriteLine($"Вызов метода {nameof(Write)} класса {nameof(PublisherToWriterAdapter)}.");
+                _filePublisher.Publish(contents);
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
